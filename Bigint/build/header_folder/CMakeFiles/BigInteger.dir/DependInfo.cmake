48182ebb8381974f9492fd77e4333e7e4f8fa8bd
swan-Dive <tnd356@yandex.ru>
2022-12-22 21:17:23 +0500

    ready

---
+++ Bigint/build/header_folder/CMakeFiles/BigInteger.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/nikita/Документы/informatics/c_homework/Bigint/header_folder/BigInt.cpp" "header_folder/CMakeFiles/BigInteger.dir/BigInt.cpp.o" "gcc" "header_folder/CMakeFiles/BigInteger.dir/BigInt.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Bigint/build/header_folder/CMakeFiles/BigInteger.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BigInteger.dir/BigInt.cpp.o"
+  "CMakeFiles/BigInteger.dir/BigInt.cpp.o.d"
+  "libBigInteger.a"
+  "libBigInteger.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BigInteger.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
